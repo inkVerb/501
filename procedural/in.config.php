ed951fdef91ea92aa07b35bd42f39945eee25a82
jessesteele <jessesteele@gmail.com>
2021-11-19 19:01:02 +0800

    PDO bugs finished

--- procedural/in.config.php
+++ procedural/in.config.php
@@ -3,6 +3,11 @@
 // Start our $_SESSION
 session_start();
 
+// In case you want to show errors
+//ini_set('display_errors', '1');
+//ini_set('display_startup_errors', '1');
+//error_reporting(E_ALL);
+
 // MySQLi config
 require_once ('./in.sql.php');
 
