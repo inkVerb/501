98592fc8c89b50bb8d5e17da40685c05b95af547
jessesteele <jessesteele@gmail.com>
2023-10-22 18:36:32 +0800

    Lesson 11 complete

--- pdo/in.db.php
+++ pdo/in.db.php
@@ -4,9 +4,9 @@
 session_start();
 
 // In case you want to show errors
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
 
 // MySQLi config
 require_once ('./in.conf.php');
